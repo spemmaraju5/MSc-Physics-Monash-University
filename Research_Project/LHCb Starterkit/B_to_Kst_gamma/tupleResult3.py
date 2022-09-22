fbe6aeb51e80012fa32f52b17a33ee612325d839
Sai Pemmaraju <saicharanpemmaraju@rocketmail.com>
2022-09-22 12:15:46 +1000

    Edited thesis draft

---
+++ Research_Project/LHCb Starterkit/B_to_Kst_gamma/tupleResult3.py
@@ -0,0 +1,84 @@
+from Configurables import LHCb__ParticlePropertySvc as ParticlePropertySvc
+from Configurables import Gauss, PrintMCTree, PrintMCDecayTreeTool, HistogramPersistencySvc, NTupleSvc, DumpHepMCDecay, DumpHepMCTree, GaussMonitor__CheckLifeTimeHepMC, GaussMonitor__CheckLifeTimeMC, GiGa, GiGaPhysListModular, GiGaHiggsParticles, GenerationToSimulation, PythiaProduction
+
+ParticlePropertySvc().Particles = ["eta                    17         221   0.0      2.000     5.063171e-19                       eta         221      0.00000000"]
+ApplicationMgr().ExtSvc    += [ ParticlePropertySvc() ]
+decay = "[[B0]nos => ^(K*(892)0 ==> K+ pi-) ^(eta => ^gamma ^gamma)]CC"
+datafile = "Gauss-11102441-10ev-20220908.xgen"
+
+mc_basic_loki_vars = {
+    'ETA' : 'MCETA',
+    'PHI' : 'MCPHI',
+    'PT' : 'MCPT',
+    'PX' : 'MCPX',
+    'PY' : 'MCPY',
+    'PZ' : 'MCPZ',
+    'E' : 'MCP',
+    'P' : 'MCP',
+    'M' : 'MCM'
+}
+
+from Configurables import (
+    DaVinci,
+    MCDecayTreeTuple
+)
+from DecayTreeTuple.Configuration import *
+
+# For a quick and dirty check, you don't need to edit anything below here.
+##########################################################################
+
+# Create an MC DTT containing any candidates matching the decay descriptor
+mctuple = MCDecayTreeTuple("MCDecayTreeTuple")
+mctuple.Decay = decay
+
+mctuple.ToolList = []
+mctuple.addTupleTool(
+    'LoKi::Hybrid::MCTupleTool/basicLoKiTT'
+).Variables = mc_basic_loki_vars
+
+# Name of the .xgen file produced by Gauss
+from GaudiConf import IOHelper
+# Use the local input data
+IOHelper().inputFiles([
+    datafile
+], clear=True)
+
+# Configure DaVinci
+DaVinci().TupleFile = "DVntuple.root"
+DaVinci().Simulation = True
+DaVinci().Lumi = False
+DaVinci().DataType = '2018'
+DaVinci().InputType = 'DIGI'
+DaVinci().UserAlgorithms = [mctuple]
+
+from Gaudi.Configuration import appendPostConfigAction
+def doIt():
+    """
+    specific post-config action for (x)GEN-files
+    """
+    extension = "xgen"
+    ext = extension.upper()
+
+    from Configurables import DataOnDemandSvc
+    dod  = DataOnDemandSvc ()
+    from copy import deepcopy
+    algs = deepcopy ( dod.AlgMap )
+    bad  = set()
+    for key in algs :
+        if     0 <= key.find ( 'Rec'     )                  : bad.add ( key )
+        elif   0 <= key.find ( 'Raw'     )                  : bad.add ( key )
+        elif   0 <= key.find ( 'DAQ'     )                  : bad.add ( key )
+        elif   0 <= key.find ( 'Trigger' )                  : bad.add ( key )
+        elif   0 <= key.find ( 'Phys'    )                  : bad.add ( key )
+        elif   0 <= key.find ( 'Prev/'   )                  : bad.add ( key )
+        elif   0 <= key.find ( 'Next/'   )                  : bad.add ( key )
+        elif   0 <= key.find ( '/MC/'    ) and 'GEN' == ext : bad.add ( key )
+
+    for b in bad :
+        del algs[b]
+
+    dod.AlgMap = algs
+
+    from Configurables import EventClockSvc, CondDB
+    EventClockSvc ( EventTimeDecoder = "FakeEventTime" )
+    CondDB  ( IgnoreHeartBeat = True )
