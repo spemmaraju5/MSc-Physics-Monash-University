a268099746a16a36e3207c0e4f27d27d6a4a2b71
Sai Pemmaraju <Sai Pemmaraju spemmara@cern.ch>
2022-12-13 14:56:13 +1100

    Edited Chapter 2 of Lit Review

---
+++ Research_Project/Bd_Ksta0,gg=DecProdCut,m=1000MeV.py
@@ -0,0 +1,50 @@
+file /afs/cern.ch/user/s/spemmara/public/GaussDev_v49r21/Gen/DecFiles/options/Bd_Ksta0,gg=DecProdCut,m=1000MeV.py generated: Tue, 22 Nov 2022 13:14:01
+#
+# Event Type: 11102462
+#
+# ASCII decay Descriptor: {[[B0]nos -> (K*(892)0 -> K+ pi-) (a0 -> gamma gamma)]cc, [[B0]os -> (K*(892)~0 -> K- pi+) (a0 -> gamma gamma)]cc}
+#
+from Configurables import Generation
+Generation().EventType = 11102462
+Generation().SampleGenerationTool = "SignalRepeatedHadronization"
+from Configurables import SignalRepeatedHadronization
+Generation().addTool( SignalRepeatedHadronization )
+Generation().SignalRepeatedHadronization.ProductionTool = "Pythia8Production"
+from Configurables import ToolSvc
+from Configurables import EvtGenDecay
+ToolSvc().addTool( EvtGenDecay )
+ToolSvc().EvtGenDecay.UserDecayFile = "$DECFILESROOT/dkfiles/Bd_Ksta0,gg=DecProdCut,m=1000.dec"
+Generation().SignalRepeatedHadronization.CutTool = "DaughtersInLHCb"
+Generation().SignalRepeatedHadronization.SignalPIDList = [ 511,-511 ]
+
+# Ad-hoc particle gun code
+
+from Configurables import ParticleGun
+pgun = ParticleGun("ParticleGun")
+pgun.SignalPdgCode = 511
+pgun.DecayTool = "EvtGenDecay"
+pgun.GenCutTool = "DaughtersInLHCb"
+
+from Configurables import FlatNParticles
+pgun.NumberOfParticlesTool = "FlatNParticles"
+pgun.addTool( FlatNParticles , name = "FlatNParticles" )
+
+from Configurables import MomentumSpectrum
+pgun.ParticleGunTool = "MomentumSpectrum"
+pgun.addTool( MomentumSpectrum , name = "MomentumSpectrum" )
+pgun.MomentumSpectrum.PdgCodes = [ 511,-511 ]
+pgun.MomentumSpectrum.InputFile = "$PGUNSDATAROOT/data/Ebeam4000GeV/MomentumSpectrum_511.root"
+pgun.MomentumSpectrum.BinningVariables = "pteta"
+pgun.MomentumSpectrum.HistogramPath = "h_pteta"
+
+from Configurables import BeamSpotSmearVertex
+pgun.addTool(BeamSpotSmearVertex, name="BeamSpotSmearVertex")
+pgun.VertexSmearingTool = "BeamSpotSmearVertex"
+pgun.EventType = 11102462
+
+
+
+
+
+
+
